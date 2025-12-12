37ef07f82653ebfb945582fa3fe72fbc96ba4d59
France Bungay <france.bungay@edu.ece.fr>
2025-12-12 14:21:52 +0100

    Affichage stylé du plateau avec symboles et couleurs ANSI

--- plateau.c
+++ plateau.c
@@ -14,9 +14,29 @@ void initialiserPlateau(int plateau[LIGNES][COLONNES]) {
 }
 
 void afficherPlateau(int plateau[LIGNES][COLONNES]) {
+
     for (int i = 0; i < LIGNES; i++) {
         for (int j = 0; j < COLONNES; j++) {
-            printf("%d ", plateau[i][j]);
+
+            switch (plateau[i][j]) {
+                case 0:
+                    printf("\033[31m■ \033[0m"); // rouge
+                    break;
+                case 1:
+                    printf("\033[32m■ \033[0m"); // vert
+                    break;
+                case 2:
+                    printf("\033[34m■ \033[0m"); // bleu
+                    break;
+                case 3:
+                    printf("\033[33m■ \033[0m"); // jaune
+                    break;
+                case 4:
+                    printf("\033[35m■ \033[0m"); // violet
+                    break;
+                default:
+                    printf("  ");
+            }
         }
         printf("\n");
     }
